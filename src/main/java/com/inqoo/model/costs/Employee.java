d0dc9dbafa8ac23984620902a8c26f79db3d045e
Tincoquesse <kamil.sound@gmail.com>
2022-04-11 23:30:11 +0200

    im using hashset of objects to store information about incomes and expenses

--- src/main/java/com/inqoo/model/costs/Employee.java
+++ src/main/java/com/inqoo/model/costs/Employee.java
@@ -3,11 +3,11 @@ package com.inqoo.model.costs;
 import lombok.Data;
 
 @Data
-public class Salary {
+public class Employee {
     private String employeeName;
     private double monthlySalary;
 
-    public Salary(String employeeName, double monthlySalary) {
+    public Employee(String employeeName, double monthlySalary) {
         this.employeeName = employeeName;
         this.monthlySalary = monthlySalary;
     }
